388f4bad47ab689882ac6d6dcf8bdb3b191048d3
ShurikuS <cawa007@gmail.com>
2016-12-06 15:43:27 +0300

    BubbleSort refactoring

--- src/java/BubbleSort.java
+++ src/java/BubbleSort.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 
 public class BubbleSort {
 
-
     private static int[] sort(int[] intArray){
         for (int i = intArray.length - 1; i >= 0; i--) {
             for (int j = 0; j < i; j++) {
@@ -18,7 +17,6 @@ public class BubbleSort {
         return intArray;
     }
 
-
     public static void main(String[] args) {
         int[] intArray = new int[]{25, 44, 23, 77, 1, 25, 44, 150};
         int[] intArraySort = sort(intArray);
@@ -27,7 +25,5 @@ public class BubbleSort {
         int[] intArray2 = new int[]{1000, 547, 750, 27, 1, 97, 300, 1001};
         int[] intArraySort2 = sort(intArray2);
         System.out.println(Arrays.toString(intArray2));
-
-
     }
 }
